da389f88002dbf8783dcf5a5a08e0adf77f7d666
mmbower1 <mttbwr91@gmail.com>
2024-06-08 23:23:24 -0700

    ui

---
+++ components/Navbar.tsx
@@ -0,0 +1,44 @@
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../img/eth-logo.png";
+
+const Navbar = () => {
+  return (
+    <div className="bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between">
+      <Link href="/">
+        <Image src={logo} alt="eth-logo" width={40} />
+      </Link>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger className="'focus:outline-none">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="avatar" />
+            <AvatarFallback className="text-black">MB</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href="/profile">Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href="/auth">Logout</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default Navbar;
